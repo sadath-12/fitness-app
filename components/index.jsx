45cdf448599c748d539ca19cc7f8b0aab15474dd
Sadath Syed <sadathsadu2002@gmail.com>
2022-06-30 12:35:19 +0530

    button removed

--- components/index.jsx
+++ components/index.jsx
@@ -1,11 +1,11 @@
 import AboutUs from "./AboutUs"
 import Banner from "./Banner"
 import BookConsultation from "./BookConsultation"
-import Button from "./Button"
+
 import Footer from "./Footer"
 import Layout from "./Layout"
 import Navbar from "./Navbar"
 import TestimonialCard from "./TestimonialCard"
 import Testimonials from "./Testimonials"
 
-export {Navbar,Layout,Banner,BookConsultation,Footer,Testimonials,Button,TestimonialCard,AboutUs}
+export {Navbar,Layout,Banner,BookConsultation,Footer,Testimonials,TestimonialCard,AboutUs}
